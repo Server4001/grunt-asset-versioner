0cf99786756de981998169d639bcce8f25be9cb1
Brice Bentler <server4001@gmail.com>
2017-02-05 22:25:10 -0800

    Adds file extension

--- tasks/asset_versioner.js
+++ tasks/asset_versioner.js
@@ -10,7 +10,6 @@
 
 const util = require('util'); // TODO : REMOVE.
 const crypto = require('crypto');
-const path = require('path');
 
 module.exports = function(grunt) {
 
@@ -18,6 +17,8 @@ module.exports = function(grunt) {
   // creation: http://gruntjs.com/creating-tasks
 
   grunt.registerMultiTask('asset_versioner', 'Versions your JS and CSS assets.', function() {
+    // TODO : Split this up into separate methods.
+
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({ // TODO : Add options.
       punctuation: '.',
@@ -37,66 +38,54 @@ module.exports = function(grunt) {
       console.log(fileGlob.orig.src); // TODO : REMOVE.
       // Find all file paths that match the glob.
       const hashes = grunt.file.expand(fileGlob.orig.src).map((filePath) => {
+        // TODO : Check if this is a file. EG:  './js/something'.
+        let fileExtension;
         let md5Hash = crypto.createHash('md5');
 
         // Create a hash based on the file name and current micro-time.
         md5Hash.update(filePath + (new Date).getTime());
 
-        // TODO : Add file extension.
+        if (filePath.substr(-7) === '.min.js') {
+          fileExtension = '.min.js';
+        } else if (filePath.substr(-8) === '.min.css') {
+          fileExtension = '.min.css';
+        } else if (filePath.substr(-3) === '.js') {
+          fileExtension = '.js';
+        } else if (filePath.substr(-4) === '.css') {
+          fileExtension = '.css';
+        } else {
+          grunt.log.error('File has unsupported file extension: ' + filePath);
+          fileExtension = '';
+        }
 
-        return md5Hash.digest('hex');
+        return md5Hash.digest('hex') + fileExtension;
       });
 
       console.log(hashes);
       // grunt.file.copy(srcpath, destpath [, options])
-      var options = {
-        // If an encoding is not specified, default to grunt.file.defaultEncoding.
-        // If null, the `process` function will receive a Buffer instead of String.
-        encoding: encodingName,
-        // The source file contents, source file path, and destination file path
-        // are passed into this function, whose return value will be used as the
-        // destination file's contents. If this function returns `false`, the file
-        // copy will be aborted.
-        process: processFunction,
-        // These optional globbing patterns will be matched against the filepath
-        // (not the filename) using grunt.file.isMatch. If any specified globbing
-        // pattern matches, the file won't be processed via the `process` function.
-        // If `true` is specified, processing will be prevented.
-        noProcess: globbingPatterns
-      };
+      // var options = {
+      //   // If an encoding is not specified, default to grunt.file.defaultEncoding.
+      //   // If null, the `process` function will receive a Buffer instead of String.
+      //   encoding: encodingName,
+      //   // The source file contents, source file path, and destination file path
+      //   // are passed into this function, whose return value will be used as the
+      //   // destination file's contents. If this function returns `false`, the file
+      //   // copy will be aborted.
+      //   process: processFunction,
+      //   // These optional globbing patterns will be matched against the filepath
+      //   // (not the filename) using grunt.file.isMatch. If any specified globbing
+      //   // pattern matches, the file won't be processed via the `process` function.
+      //   // If `true` is specified, processing will be prevented.
+      //   noProcess: globbingPatterns
+      // };
 
       // grunt.file.delete(filepath [, options])
-      var options = {
-        // Enable deleting outside the current working directory. This option may
-        // be overridden by the --force command-line option.
-        force: true
-      };
+      // var options = {
+      //   // Enable deleting outside the current working directory. This option may
+      //   // be overridden by the --force command-line option.
+      //   force: true
+      // };
     });
-
-    // // Iterate over all specified file groups.
-    // this.files.forEach(function(f) {
-    //   // Concat specified files.
-    //   var src = f.orig.src.filter(function(filepath) {
-    //     // Warn on and remove invalid source files (if nonull was set).
-    //     if (!grunt.file.exists(filepath)) {
-    //       grunt.log.warn('Source file "' + filepath + '" not found.');
-    //       return false;
-    //     } else {
-    //       return true;
-    //     }
-    //   }).map(function(filepath) {
-    //     // Read file source.
-    //     return grunt.file.read(filepath);
-    //   }).join(grunt.util.normalizelf(options.separator));
-    //
-    //   // Handle options.
-    //   src += options.punctuation;
-    //
-    //   // Write the destination file.
-    //
-    //   // Print a success message.
-    //   grunt.log.writeln('File "' + f.dest + '" created.');
-    // });
   });
 
 };
